3665681bae9c0f385efac480c7e6b3448c4d0ad9
roninethra <74942212+roninethra@users.noreply.github.com>
2021-09-11 14:22:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ function setup(){
 
     video= createCapture(VIDEO);
      video.hide();
+     PoseNet= ml5.poseNet(video, modelloaded);
+     PoseNet.on("pose", gotPoses);
 }
 
 function draw(){
     image(video, 0, 0, 600, 500);
+}
+
+function modelloaded(){
+console.log("You finally got here!")
 }
 
 function preload(){
@@ -17,4 +23,20 @@ function preload(){
 
 function play(){
     song.play();
+    song.setVolume();
+    song.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+        console.log(leftWristX +" "+ leftWristY +" "+ rightWristX +" "+ rightWristY);
+    }
+    else{
+        
+    }
 }
